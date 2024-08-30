468d054a93a3b295857c6615005ae9999f2ff9b6
kde <kdeun1@naver.com>
2024-08-31 02:22:07 +0900

    refact: 원티드 필터링 코드 리펙토링 및 UX 개선

--- content.js
+++ content.js
@@ -1,134 +1,162 @@
-function filterCards() {
-  chrome.storage.local.get('wanted_filter_company_name', result => {
-    if (result?.wanted_filter_company_name) {
-      const companyArr = result.wanted_filter_company_name;
-      if (companyArr?.length > 0) {
-        const cards = document.querySelectorAll('[class*="Card_Card__"]');
+const EXCEPT_KEYWORD = 'except_keyword';
+const WANTED_CHECK = 'wanted_check';
+const DELAY_TIMEOUT = 500;
 
-        cards.forEach(card => {
-          const companyName =
-            card.children[0].children[0].getAttribute('data-company-name');
-          const positionName =
-            card.children[0].children[0].getAttribute('data-position-name');
-          for (let keyword of companyArr) {
-            const trimedKeyword = keyword.trim();
-            if (
-              companyName.includes(trimedKeyword) ||
-              positionName.includes(trimedKeyword)
-            ) {
-              card.style.display = 'none';
-              break;
-            }
-          }
-        });
-      }
-    }
-  });
-}
+const debounce = (func, timeout = 300) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
 
-const observer = new MutationObserver(mutationList => {
-  chrome.storage.local.get('wanted_filter_check', result => {
-    if (result?.wanted_filter_check === true) {
-      for (const mutation of mutationList) {
-        if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-          filterCards();
-        }
-      }
-    }
-  });
-});
-
-function startObserver() {
-  const jobListUl = document.querySelectorAll('[class*="List_List__"]')[0];
-  observer.observe(jobListUl, {
-    subtree: true,
-    childList: true,
-  });
-}
-
-function endObserver() {
-  observer.disconnect();
-}
-
-function saveKeywordsToLocalStorage(keywords) {
-  chrome.storage.local.set({ wanted_filter_company_name: keywords }, () => {
-    console.log('Keywords saved to local storage:', keywords);
-  });
-}
-
-function cancelFilterCards() {
-  const cards = document.querySelectorAll('[class*="Card_Card__"]');
-
-  cards.forEach(card => {
-    card.style.display = '';
-  });
-}
-
-function clearStorage(key) {
-  let removedKeys = ['wanted_filter_company_name, wanted_filter_check'];
-  if (key) {
-    removedKeys = [key];
+const splitKeywordToArray = keyword => {
+  if (!keyword) {
+    return [];
   }
-  chrome.storage.local.remove(removedKeys, () => {
-    console.log('Keywords removed to chrome storage');
-  });
-}
+  return (
+    String(keyword)
+      .split(/,|\r\n|\r|\n|\t/)
+      ?.map(v => v.trim())
+      ?.filter(v => v) ?? []
+  );
+};
 
-function saveCheckToStorage(flag) {
-  chrome.storage.local.set({ wanted_filter_check: flag }, () => {
-    console.log('Checking filter');
-  });
-}
+const filterCards = () => {
+  chrome.storage.local.get('except_keyword', ({ except_keyword }) => {
+    const wdlistCards = document.querySelectorAll('[class*="Card_Card__"]');
+    const keywordArr = splitKeywordToArray(except_keyword);
 
-window.addEventListener('popstate', function () {
-  setTimeout(() => {
-    chrome.storage.local.get('wanted_filter_check', result => {
-      if (result?.wanted_filter_check === true) {
-        startObserver();
-        filterCards();
-      }
-    });
-  }, 500);
-});
+    if (keywordArr?.length > 0) {
+      wdlistCards.forEach(card => {
+        const cardInner = card.children[0].children[0];
+        const wantedCompanyName = cardInner
+          .getAttribute('data-company-name')
+          .toLowerCase();
+        const wantedPositionName = cardInner
+          .getAttribute('data-position-name')
+          .toLowerCase();
 
-window.onload = function () {
-  chrome.storage.local.get('wanted_filter_check', result => {
-    if (result?.wanted_filter_check === true) {
-      startObserver();
-      filterCards();
+        if (
+          keywordArr.some(
+            keyword =>
+              wantedCompanyName.includes(keyword.toLowerCase()) ||
+              wantedPositionName.includes(keyword.toLowerCase())
+          )
+        ) {
+          card.style.display = 'none';
+        }
+      });
+    } else {
+      wdlistCards.forEach(card => {
+        card.style.display = 'none';
+      });
     }
   });
 };
 
+window.addEventListener('popstate', () => {
+  setTimeout(() => {
+    chrome.storage.local.get('wanted_check', ({ wanted_check }) => {
+      if (wanted_check) {
+        filterCards();
+      }
+    });
+  }, DELAY_TIMEOUT);
+});
+
+let wantedObserver = null;
+
+window.onload = () => {
+  chrome.storage.local.get('wanted_check', ({ wanted_check }) => {
+    if (wanted_check) {
+      filterCards();
+      startObserver();
+    } else {
+      disconnectObserver();
+    }
+  });
+
+  wantedObserver = new MutationObserver(
+    debounce(mutationList => {
+      chrome.storage.local.get('wanted_check', ({ wanted_check }) => {
+        if (wanted_check) {
+          filterCards();
+        }
+      });
+    }, DELAY_TIMEOUT)
+  );
+};
+
+const startObserver = () => {
+  if (wantedObserver) {
+    const jobListUl = document.querySelectorAll('[class*="List_List__"]')[0];
+    if (jobListUl) {
+      wantedObserver.observe(jobListUl, {
+        childList: true,
+      });
+    }
+  }
+};
+
+const disconnectObserver = () => {
+  if (wantedObserver) {
+    wantedObserver.disconnect();
+  }
+};
+
+const saveValueToLocalStorage = obj => {
+  chrome.storage.local.set(obj, () => {
+    console.log('Keywords saved to local storage:', obj);
+  });
+};
+
+const initCardsStyle = () => {
+  const wdlistCards = document.querySelectorAll('[class*="Card_Card__"]');
+
+  wdlistCards.forEach(cardInner => {
+    cardInner.style.display = '';
+  });
+};
+
+const clearAllLocalStorage = () => {
+  let removedKeys = ['except_keyword, wanted_check'];
+  chrome.storage.local.remove(removedKeys, () => {
+    console.log('Keywords removed to chrome storage');
+  });
+};
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'filter') {
-    cancelFilterCards();
-    const keywords = request.keyword.split(',').filter(v => v);
-    if (request.keyword) {
-      saveKeywordsToLocalStorage(keywords);
-      startObserver();
+  const { action, keyword } = request;
+
+  switch (action) {
+    case 'wantedCheck': {
+      saveValueToLocalStorage({ wanted_check: true });
+      if (keyword) {
+        saveValueToLocalStorage({ except_keyword: keyword });
+      }
       filterCards();
-    } else {
-      clearStorage('wanted_filter_company_name');
-    }
-    sendResponse({ status: 'done' });
-  } else if (request.action === 'reset') {
-    endObserver();
-    cancelFilterCards();
-    sendResponse({ status: 'done' });
-  } else if (request.action === 'clear') {
-    cancelFilterCards();
-    clearStorage();
-    sendResponse({ status: 'done' });
-  } else if (request.action === 'check') {
-    saveCheckToStorage(request.isChecked);
-    if (request.isChecked) {
       startObserver();
-      filterCards();
-    } else {
-      endObserver();
-      cancelFilterCards();
+
+      sendResponse({ status: 'done' });
+      break;
     }
-    sendResponse({ status: 'done' });
+    case 'wantedUncheck': {
+      saveValueToLocalStorage({ wanted_check: false });
+      initCardsStyle();
+      disconnectObserver();
+
+      sendResponse({ status: 'done' });
+      break;
+    }
+    case 'clearButtonClick': {
+      clearAllLocalStorage();
+      break;
+    }
+    default: {
+      break;
+    }
   }
 });
--- popup.js
+++ popup.js
@@ -1,61 +1,41 @@
-const filterButton = document.getElementById('filterButton');
-const resetButton = document.getElementById('resetButton');
-const clearButton = document.getElementById('clearButton');
-const filterCheckbox = document.getElementById('filterCheckbox');
 const keywordTextarea = document.getElementById('keyword');
+const wantedFilterCheckbox = document.getElementById('wantedFilterCheckbox');
+const clearButton = document.getElementById('clearButton');
 
 document.addEventListener('DOMContentLoaded', () => {
-  chrome.storage.local.get('wanted_filter_company_name', result => {
-    if (result?.wanted_filter_company_name) {
-      keywordTextarea.value = result.wanted_filter_company_name;
-    }
-  });
-
-  chrome.storage.local.get('wanted_filter_check', result => {
-    if (result?.wanted_filter_check === true) {
-      filterCheckbox.checked = result.wanted_filter_check;
-    }
-  });
-});
-
-filterButton.addEventListener('click', () => {
-  const keyword = keywordTextarea.value;
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    chrome.tabs.sendMessage(
-      tabs[0].id,
-      { action: 'filter', keyword: keyword },
-      response => {
-        console.log(response?.status);
+  chrome.storage.local.get(
+    ['wanted_check', 'except_keyword'],
+    ({ wanted_check, except_keyword }) => {
+      if (wanted_check) {
+        wantedFilterCheckbox.checked = wanted_check;
       }
-    );
-  });
+      if (except_keyword) {
+        keywordTextarea.value = except_keyword;
+      }
+    }
+  );
 });
 
-resetButton.addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'reset' }, response => {
+const TABS_QUERY_PARAMS = { active: true, currentWindow: true };
+
+wantedFilterCheckbox.addEventListener('change', () => {
+  const isChecked = wantedFilterCheckbox.checked;
+  const action = isChecked ? 'wantedCheck' : 'wantedUncheck';
+  const keyword = keywordTextarea.value;
+
+  chrome.tabs.query(TABS_QUERY_PARAMS, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, { action, keyword }, response => {
       console.log(response?.status);
     });
   });
 });
 
 clearButton.addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'clear' }, response => {
+  const action = 'clearButtonClick';
+
+  chrome.tabs.query(TABS_QUERY_PARAMS, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, { action }, response => {
       console.log(response?.status);
     });
-  });
-});
-
-filterCheckbox.addEventListener('change', () => {
-  const isChecked = filterCheckbox.checked;
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    chrome.tabs.sendMessage(
-      tabs[0].id,
-      { action: 'check', isChecked: isChecked },
-      response => {
-        console.log(response?.status);
-      }
-    );
   });
 });
